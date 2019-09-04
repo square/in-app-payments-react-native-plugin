84f9b6cd02867c8d00ac39f7754d84c48977aec2
Xingfu Dong <xingfu@squareup.com>
2019-09-04 15:09:38 -0700

    fix yarn lint issue

--- react-native-in-app-payments-quickstart/rn-cli.config.js
+++ react-native-in-app-payments-quickstart/rn-cli.config.js
@@ -1,8 +1,9 @@
 const blacklist = require('metro-config/src/defaults/blacklist');
+
 module.exports = {
-    resolver: {
-        blacklistRE: blacklist([
-            /node_modules\/.*\/node_modules\/react-native\/.*/,
-        ])
-    },
+  resolver: {
+    blacklistRE: blacklist([
+      /node_modules\/.*\/node_modules\/react-native\/.*/,
+    ]),
+  },
 };
