cac30c5b545ee4d22caa7d593669823bed380bb1
Vishwajit Shep <vishwajit.shep@saviantconsulting.com>
2021-05-19 02:24:32 +0530

    Updated react-native and it's dependencies for android

--- react-native-in-app-payments-quickstart/android/app/src/main/java/sqip/react/example/MainActivity.java
+++ react-native-in-app-payments-quickstart/android/app/src/main/java/sqip/react/example/MainActivity.java
@@ -1,6 +1,9 @@
 package sqip.react.example;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
     /**
@@ -10,5 +13,15 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "RNInAppPaymentsQuickstart";
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+          @Override
+          protected ReactRootView createRootView() {
+            return new RNGestureHandlerEnabledRootView(MainActivity.this);
+          }
+        };
     }
 }
