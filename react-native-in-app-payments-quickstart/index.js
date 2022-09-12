829b3fb9519171f29100f859b5b0cbfdbc5dbf5b
charmisG <102352276+CharmisG@users.noreply.github.com>
2022-09-12 10:42:59 +0530

    Update index.js

--- react-native-in-app-payments-quickstart/index.js
+++ react-native-in-app-payments-quickstart/index.js
@@ -14,7 +14,7 @@
  limitations under the License.
 */
 
-import { AppRegistry ,LogBox } from 'react-native';
+import { AppRegistry, LogBox } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
