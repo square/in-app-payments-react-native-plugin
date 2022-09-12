c9c1283223a01625f5b8dedb4535bff4d3ce23d0
charmisG <102352276+CharmisG@users.noreply.github.com>
2022-09-12 10:45:28 +0530

    Update index.js

--- react-native-in-app-payments-quickstart/index.js
+++ react-native-in-app-payments-quickstart/index.js
@@ -18,5 +18,5 @@ import { AppRegistry, LogBox } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
-LogBox.ignoreAllLogs(); 
+LogBox.ignoreAllLogs();
 AppRegistry.registerComponent(appName, () => App);
