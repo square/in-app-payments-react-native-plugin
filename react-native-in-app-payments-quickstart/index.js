92af508563157ccaee9fb0cc49417f3adcdd129a
charmisG <102352276+CharmisG@users.noreply.github.com>
2022-09-19 14:09:42 +0530

    Update index.js

--- react-native-in-app-payments-quickstart/index.js
+++ react-native-in-app-payments-quickstart/index.js
@@ -20,5 +20,7 @@ import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
 
-// We are getting warnings due to the newest version of react-native. A lot of libraries still haven't released a new version to handle these errors (or warnings in some cases). so we can hide the warning for now with this logBox.
+/* We are getting warnings due to the newest version of react-native.
+A lot of libraries still haven't released a new version to handle these errors (or warnings in some cases). 
+so we can hide the warning for now with this logBox. */
 LogBox.ignoreAllLogs();
