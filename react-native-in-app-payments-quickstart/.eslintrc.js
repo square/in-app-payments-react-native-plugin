2f0d8d8f0aacda1b625cbe49bf3b741d9106f2fb
mansi-square <mdabhi@squareup.com>
2024-05-27 10:17:33 +0530

    Update .eslintrc.js

--- react-native-in-app-payments-quickstart/.eslintrc.js
+++ react-native-in-app-payments-quickstart/.eslintrc.js
@@ -1,17 +1,15 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions:{
-    project: './tsconfig.json',
-    createDefaultProgram: true,
-  },
+  parser: 'babel-eslint',
+  root: true,
   extends: [
-    'airbnb-typescript',
+    'airbnb',
     'plugin:react-native/all',
   ],
   settings: {
     'import/resolver': {
       node: {
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', './'],
       },
     },
   },
@@ -21,7 +19,6 @@ module.exports = {
   ],
   env: {
     'react-native/react-native': true,
-    jest: true,
   },
   rules: {
     'react-native/no-unused-styles': 2,
@@ -34,5 +31,8 @@ module.exports = {
     'no-console': 0,
     'react/forbid-prop-types': 0,
     'class-methods-use-this': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/extensions': 0,
+    'import/no-unresolved': 2,
   },
 };
